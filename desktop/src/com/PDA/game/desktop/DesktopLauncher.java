890d72f914d615b3de9750076fa71d2f27d00ff8
Peter <peter800902@hotmail.com>
2014-04-25 11:20:15 +0800

    first commit

---
+++ desktop/src/com/PDA/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.PDA.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.PDA.game.MyPDAGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyPDAGame(), config);
+	}
+}
